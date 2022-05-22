bdcb8eb38ca247181e38862b914163990c1f6441
FrankMachuca <franciscomachucaugb2020@gmail.com>
2022-05-21 21:04:21 -0600

    Funcion y apariencia del Login

--- AAE/resources/views/auth/login.blade.php
+++ AAE/resources/views/auth/login.blade.php
@@ -1,73 +1,96 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Correo') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+<div id="login-jumbotron" class="jumbotron jumbotron-fluid">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Contraseña') }}</label>
+    <div class="flex-container-space-between">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+        <div id="arrow-button">
+            <a id="arrow-button" class="bi bi-arrow-left" href="{{ url('/') }}"></a>
+        </div>
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+        <div>
+            <div id="title" class="header" style="font-size: 30px; color: #FFFFFF">Ask & Answer Education</div>
+        </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+        <div>
+            <h2 style="color: #4F14A0;">Help</h2>
         </div>
     </div>
+
+
+    <div class="flex-container">
+
+
+        <div>
+
+            <div id="login-title">
+                <h1 style="font-size: 70px; color: #FFFFFF">Iniciar <br />
+                    Sesión</h1>
+                <p style="font-size: 15px; color: #BEA6ED">¿No tiene una cuenta? <a href="{{ route('register') }}">Regístrese</a></p>
+            </div>
+        </div>
+
+        <div>
+
+            <div id="login-form" class="container-fluid">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <div id="login-control">
+                        <input id="email" type="email" class="form-control-fluid form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                        @error('email')
+                        <span class="invalid-feedback">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+
+                    <div id="login-control">
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+
+                    <div id="login-control">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                            <label style="color: #FFFFFF" class="form-check-label" for="remember">
+                                {{ __('Recuerdame') }}
+                            </label>
+                        </div>
+                    </div>
+
+                    <div id="login-button">
+                        <button id="login-button" type="submit" class="btn btn-primary">
+                            {{ __('Iniciar Sesión') }}
+                        </button>
+
+                        @if (Route::has('password.request'))
+                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                            {{ __('¿Olvidaste tu contraseña?') }}
+                        </a>
+                        @endif
+                    </div>
+                </form>
+            </div>
+
+        </div>
+
+
+    </div>
+
 </div>
+
+
 @endsection
--- AAE/resources/views/layouts/app.blade.php
+++ AAE/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('AA&E', 'A&AE') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -33,10 +33,10 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('Ask & Asnwer Education', 'Ask & Asnwer Education') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
